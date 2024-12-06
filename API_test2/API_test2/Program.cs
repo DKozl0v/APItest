1002822635532f805db536ad8841758df1ac25dc
Гость <@b304-12.ygk.ru>
2024-12-06 14:09:54 +0300

    my first commit

---
+++ API_test2/API_test2/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net;
+
+namespace API_test2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string URL = "https://reqres.in/api/users/1";
+            var request = (HttpWebRequest)WebRequest.Create(URL);
+            
+            request.Method = "GET";
+            request.Proxy.Credentials = new NetworkCredential("student", "student");
+            var response = (HttpWebResponse)request.GetResponse();
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            string Text = reader.ReadToEnd();
+            var JsonText = JsonConvert.DeserializeObject<User>(Text);
+            Console.WriteLine($"{JsonText.data.id} {JsonText.data.last_name} {JsonText.data.first_name} {JsonText.data.email}");
+            Console.ReadLine();
+        }
+
+    }
+}
